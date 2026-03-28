d7fa358cf82c3b2eb3189757f544df64e1c612a2
ajithpg <ajithpg008@gmail.com>
2026-03-28 14:29:24 +0300

    fix:type issue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import SearchSection from "./_components/SearchSection";
 import TemplateList from "./_components/TemplateList";
 
 const Dashboard = () => {
-  const [userSearchInput, setUserSearchInput] = useState<string>();
+  const [userSearchInput, setUserSearchInput] = useState<string>("");
   const onSearchInput = (value: string) => {
     setUserSearchInput(value);
   };
